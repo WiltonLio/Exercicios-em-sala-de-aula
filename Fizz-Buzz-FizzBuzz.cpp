5e4ea486d4d74a84ba93e8e4ff98bb3ccbe2fe7d
WiltonLio <wilton.lio@gmail.com>
2023-09-25 08:36:31 -0300

    update

--- Fizz-Buzz-FizzBuzz.cpp
+++ Fizz-Buzz-FizzBuzz.cpp
@@ -4,7 +4,14 @@ using namespace std;
 
 int main()
 {
+    int a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z;
+
+    cout<<"Enter 20 numbers: "<<endl;
+
+    cin>>a>>b>>c>>d>>e>>f>>g>>h>>i>>j>>k>>l>>m>>n>>o>>p>>q>>r>>s>>t>>u>>v>>w>>x>>y>>z;
+    cout<<"The sum of the numbers is: "<<a+b+c+d+e+f+g+h+i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z;
     
+
 
     return 0;    
 }
--- charTypeRev.cpp
+++ charTypeRev.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <locale>
 #include <wchar.h>
-#include <cstdlib>
 
 
 using namespace std;
 
 int main(void)
 {
+
     setlocale(LC_ALL, "pt_BR.utf-8");
     locale::global(locale("pt_BR.utf-8"));
     wcout.imbue(locale("pt_BR.utf-8"));
