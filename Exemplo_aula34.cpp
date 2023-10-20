b3a1aa23bbff3f0c3e69bfa60aabcb1ef1cef486
WiltonLio <wilton.lio@gmail.com>
2023-10-20 08:54:43 -0300

    up

---
+++ Exemplo_aula34.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class ObjetoGeometrico
+{
+public:
+    virtual double getArea();
+    virtual double getPerimetro();
+};
+
+class Retangulo : public ObjetoGeometrico
+{
+private:
+    double a;
+    double b;
+
+public:
+    Retangulo(double a, double b)
+};
+
+int main(){
+    ObjetoGeometrico* obj = new ObjetoGeometrico();
+    double area, peri;
+    area = obj->getArea();
+    peri = obj->getPerimetro();
+}
+
